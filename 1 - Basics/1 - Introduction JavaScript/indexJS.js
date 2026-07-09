c0c16b795ba02d26f9f5c72856b9a3a60ed6e7db
Nattawat Lertchidumrongsri <NattawatLert0747@gmail.com>
2026-07-09 13:28:42 +0700

    feat: add Introductions JavaScript

---
+++ 1 - Basics/1 - Introduction JavaScript/indexJS.js
@@ -0,0 +1,16 @@
+console.log("Hello");
+console.log("I link pizza!");
+
+window.alert("This is an alert!")
+
+document.getElementById("myH1").textContent = "Hello";
+document.getElementById("myP").textContent = "I like pizza!";
+
+// This is comment
+
+/*
+   This
+   is
+   s
+   comment
+*/
